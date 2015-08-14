3ec528c4f1f2cac36288f7cf7fdc79c991c6a47d
joshua bowles <bowlesling@gmail.com>
2015-08-13 20:23:13 -0600

    experimenting again

---
+++ bucket_knn_example.go
@@ -0,0 +1,161 @@
+/*
+* Hotel Provider Error Builder
+* TEMPROARY: move it to the smallgear project.
+ */
+package wordlab
+
+// Labels should be distributed pretty far apart for knn algorithms to get accuracy.
+// For example, in one implementation, shifting the iota (103,206,412,824,...,13184) instead of incrementing by 1 (1,2,3,4,...,8) boosted
+// accuracy from low 70% to 99%
+/*
+const (
+	AvailID = 1 << iota * 103
+	BookID
+	CancelID
+	CancelForbidID
+	CreditDataID
+	creditDeclineID
+	CreditServiceID
+	UnexpectID
+)
+*/
+
+const (
+	AvailID         = 20
+	BookID          = 60
+	CancelID        = 100
+	CancelForbidID  = 140
+	CreditDataID    = 180
+	creditDeclineID = 220
+	CreditServiceID = 260
+	UnexpectID      = 300
+)
+
+var HotelRootFp = "/Users/jbowles/x/training_data/partner_fusion_trained_errors/training_data/"
+var HotelErrorIDTableDirs = map[int][]string{
+	AvailID:         {"availability_error", HotelRootFp + "availability_error", "datasets/tmpavail.csv"},
+	BookID:          {"booking_error", HotelRootFp + "booking_error", "datasets/tmpbook.csv"},
+	CancelID:        {"cancel_error", HotelRootFp + "cancel_error", "datasets/tmpcancel.csv"},
+	CancelForbidID:  {"cancel_forbidden_error", HotelRootFp + "cancel_forbidden_error", "datasets/tmpcnlfrbd.csv"},
+	CreditDataID:    {"credit_data_error", HotelRootFp + "credit_data_error", "datasets/tmpcrdat.csv"},
+	creditDeclineID: {"credit_decline_error", HotelRootFp + "credit_decline_error", "datasets/tmpcrdecl.csv"},
+	CreditServiceID: {"credit_service_error", HotelRootFp + "credit_service_error", "datasets/tmpcserv.csv"},
+	UnexpectID:      {"unexpected_response_error", HotelRootFp + "unexpected_response_error", "datasets/tmpunex.csv"},
+}
+
+var HotelErrorIDTableFiles = map[int][]string{
+	AvailID:         {"availability_error", HotelRootFp + "availability_error/availability_error.csv", "datasets/tmpavail.csv"},
+	BookID:          {"booking_error", HotelRootFp + "booking_error/booking_error.csv", "datasets/tmpbook.csv"},
+	CancelID:        {"cancel_error", HotelRootFp + "cancel_error/cancel_error.csv", "datasets/tmpcancel.csv"},
+	CancelForbidID:  {"cancel_forbidden_error", HotelRootFp + "cancel_forbidden_error/cancel_forbidden_error.csv", "datasets/tmpcnlfrbd.csv"},
+	CreditDataID:    {"credit_data_error", HotelRootFp + "credit_data_error/credit_data_error.csv", "datasets/tmpcrdat.csv"},
+	creditDeclineID: {"credit_decline_error", HotelRootFp + "credit_decline_error/credit_decline_error.csv", "datasets/tmpcrdecl.csv"},
+	CreditServiceID: {"credit_service_error", HotelRootFp + "credit_service_error/credit_service_error.csv", "datasets/tmpcserv.csv"},
+	UnexpectID:      {"unexpected_response_error", HotelRootFp + "unexpected_response_error/unexpected_response_error.csv", "datasets/tmpunex.csv"},
+}
+var HotelErrorNameTable = map[string]int{
+	"availability_error":        AvailID,
+	"booking_error":             BookID,
+	"cancel_error":              CancelID,
+	"cancel_forbidden_error":    CancelForbidID,
+	"credit_data_error":         CreditDataID,
+	"credit_decline_error":      creditDeclineID,
+	"credit_service_error":      CreditServiceID,
+	"unexpected_response_error": UnexpectID,
+}
+
+func AveragedLabelId() int {
+	sum := int(0)
+	for id, _ := range HotelErrorIDTableFiles {
+		sum += id
+	}
+	return (sum / (len(HotelErrorIDTableFiles) * len(HotelErrorIDTableFiles)))
+}
+
+var wordModelHeaders = CreateByteRangeHeaders(ByteRangeWordModelLimit)
+var SentModelHeaders = CreateByteRangeHeaders(ByteRangeSentModelLimit)
+var wordsBucketNameLast = "wordlab_bucket_hotel_error_words_labelnamelast_train.csv"
+var wordsBucketIdFirst = "wordlab_bucket_hotel_error_words_labelidfirst_train.csv"
+var sentsBucketNameLast = "wordlab_bucket_hotel_error_sents_labelnamelast_train.csv"
+var sentsBucketIdFirst = "wordlab_bucket_hotel_error_sents_labelidfirst_train.csv"
+
+func BuildHotelProviderDataKnnLabelNameLast(root_datafp string) {
+	new_word_hdrs := append(wordModelHeaders, "labelname")
+
+	new_sent_hdrs := append(SentModelHeaders, "LableName")
+	//new_sent_hdrs := append(SentModelHeaders, "Hashing")
+	//new_sent_hdrs = append(new_sent_hdrs, "LableName")
+
+	CsvCreateFileWithHeaders(true, (root_datafp + wordsBucketNameLast), new_word_hdrs)
+	CsvCreateFileWithHeaders(true, (root_datafp + sentsBucketNameLast), new_sent_hdrs)
+
+	for id, table := range HotelErrorIDTableFiles {
+		// add word label name last
+		wmodel := &WordModel{
+			InputFilePath:  table[1],
+			OutputFilePath: root_datafp + wordsBucketNameLast,
+			LabelName:      table[0],
+			Tokenizer:      "unicode",
+			LabelID:        id,
+			ForceOverwrite: true,
+			LabelFirst:     false,
+			LabelNameFirst: false,
+			AddLabelName:   true,
+			AddLabelID:     false,
+		}
+
+		// add sentence label name last
+		smodel := &SentenceModel{
+			InputFilePath:  table[1],
+			OutputFilePath: root_datafp + sentsBucketNameLast,
+			LabelName:      table[0],
+			Tokenizer:      "unicode",
+			LabelID:        id,
+			ForceOverwrite: true,
+			LabelFirst:     false,
+			LabelNameFirst: false,
+			AddLabelName:   true,
+			AddLabelID:     false,
+		}
+
+		smodel.ParseInputWriteOut()
+		wmodel.ParseInputWriteOut()
+	}
+}
+
+func BuildHotelProviderDataKnnLabelIdFirst(root_datafp string) {
+	new_idword_hdrs := ConcatStringSlice([]string{"LabelId"}, wordModelHeaders)
+	new_idsent_hdrs := ConcatStringSlice([]string{"LabelId"}, SentModelHeaders)
+	//new_idsent_hdrs = append(new_idsent_hdrs, "Hashing")
+
+	CsvCreateFileWithHeaders(true, (root_datafp + wordsBucketIdFirst), new_idword_hdrs)
+	CsvCreateFileWithHeaders(true, (root_datafp + sentsBucketIdFirst), new_idsent_hdrs)
+
+	for id, table := range HotelErrorIDTableFiles {
+		wmodel := &WordModel{
+			InputFilePath:  table[1],
+			OutputFilePath: root_datafp + wordsBucketIdFirst,
+			LabelName:      table[0],
+			Tokenizer:      "unicode",
+			LabelID:        id,
+			ForceOverwrite: true,
+			LabelFirst:     true,
+			AddLabelName:   false,
+			LabelNameFirst: false,
+		}
+		smodel := &SentenceModel{
+			InputFilePath:  table[1],
+			OutputFilePath: root_datafp + sentsBucketIdFirst,
+			LabelName:      table[0],
+			Tokenizer:      "unicode",
+			LabelID:        id,
+			ForceOverwrite: true,
+			LabelFirst:     true,
+			AddLabelName:   false,
+			LabelNameFirst: false,
+		}
+
+		smodel.ParseInputWriteOut()
+		wmodel.ParseInputWriteOut()
+	}
+}
---
+++ main/main.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jbowles/wordlab"
+	"runtime"
+	"time"
+	//"strings"
+	//tkz "github.com/jbowles/nlpt_tkz"
+	"github.com/sjwhitworth/golearn/base"
+	"github.com/sjwhitworth/golearn/evaluation"
+	"github.com/sjwhitworth/golearn/knn"
+	//"strconv"
+)
+
+func main() {
+	//pipeFile()
+	//pipeDir()
+	//pipeDirOpt()
+	streamDirOpt()
+	//HotelData()
+	//wordlab.AmitKnnValidate()
+}
+
+func streamDirOpt() {
+	runtime.GOMAXPROCS(8)
+	tkzr := "unicode"
+	for _, value := range wordlab.News {
+		go wordlab.StreamTokenizedDirectory(value[1], value[2], tkzr, time.Minute*90)
+	}
+	var input string
+	fmt.Scanln(&input)
+}
+
+//go run main/main.go  290.31s user 2.67s system 673% cpu 43.495 total
+//go run main/main.go  145.14s user 1.09s system 693% cpu 21.080 total
+func pipeDirOpt() {
+	runtime.GOMAXPROCS(8)
+	tkzr := "lex"
+	for _, value := range wordlab.News {
+		go wordlab.PipeTokenizedDirectoryOpt(value[1], value[2], tkzr, time.Minute*90)
+	}
+	var input string
+	fmt.Scanln(&input)
+}
+
+func pipeDir() {
+	//wordlab.CsvCreateFileWithHeaders(true, "attributes.csv", []string{"vetor", "index", "dotproduct", "label"})
+	runtime.GOMAXPROCS(8)
+	tkzr := "lex"
+	//tfidfFile := "datasets/tfidf.txt"
+	//before goroutines this took == minutes to run against 2 directories of about 1000 files each.
+	// goroutines this took == minutes to run against 2 directories of about 1000 files each.
+	for _, value := range wordlab.News {
+		wordlab.PipeTokenizedDirectory(value[1], value[2], tkzr, time.Minute*90)
+	}
+
+	/*
+		for _, value := range wordlab.HotelErrorIDTableDirs {
+			wordlab.PipeTokenizedDirectory(value[1], value[2], tkzr, time.Second*90)
+		}
+	*/
+}
+
+func pipeFile() {
+	path := "/Users/jbowles/x/training_data/corpora/20news-18828/alt.atheism/51060"
+	tkzr := "lex"
+	//tkzr := "unicode"
+	res, err := wordlab.PipeTokenizedFile(path, tkzr)
+	fmt.Println(string(res))
+	fmt.Println(err)
+}
+
+func printHotelTable() {
+	for id, s := range wordlab.HotelErrorIDTableFiles {
+		fmt.Printf("\n HotelIds: %v, Errors: %v\n", id, s[0])
+	}
+}
+
+func HotelData() {
+	var root_datafp = "datasets/"
+	wordlab.BuildHotelProviderDataKnnLabelIdFirst(root_datafp)
+	wordlab.BuildHotelProviderDataKnnLabelNameLast(root_datafp)
+}
+
+// for word model... yck
+/*
+func amitClassify() {
+	var stopList = wordlab.StopWords("datasets/stopwords/stopwords.txt")
+	sent := "We’re sorry but we were unable to process your request. You may have temporarily exceeded your credit or debit card limit. Please choose a different card and try again"
+	tokens, _ := tkz.TokenizeStr(sent, "unicode")
+	buckets := []wordlab.WordBucket{}
+	for _, token := range tokens {
+		if !stopList.IsStopWord[token] {
+			buckets = append(buckets, *wordlab.NewPredictionWordBucket(token))
+		}
+	}
+	for _, bucket := range buckets {
+		attrs := []float32{}
+		for _, bpc := range bucket.Bucket {
+			attrs = append(attrs, float32(bpc.ByteCharacter))
+		}
+		res := wordlab.AmitClassify(attrs)
+		i, _ := strconv.Atoi(string(res))
+		fmt.Printf("result for %v: %v %v\n", bucket.Word, string(res), wordlab.HotelErrorIDTableFiles[i][0])
+	}
+	//attrs := []float32{102, 97, 105, 108, 101, 100, 0, 0, 0, 0, 0, 0, 0.01652997688235604}
+	//result := wordlab.AmitClassify(attrs)
+	//fmt.Printf("result: %v\n", string(result))
+	//fmt.Printf("result: %v\n", attrs)
+}
+*/
+
+func makePrediction() {
+	write_filep := "predict_dummy_file.csv"
+	headers := wordlab.SentModelHeaders
+	//headers = append(headers, "LabelId")
+	headers = append(headers, "LabelName")
+	wordlab.CsvCreateFileWithHeaders(true, write_filep, headers)
+	var root_errorfp = "/Users/jbowles/x/training_data/partner_fusion_trained_errors/training_data/"
+
+	var HotelErrorIDTable = map[int][]string{
+		wordlab.AvailID: {"availability_error", "availability_error/availability_error.csv"},
+		wordlab.BookID:  {"booking_error", "booking_error/booking_error.csv"},
+	}
+	for id, table := range HotelErrorIDTable {
+		smodel := &wordlab.SentenceModel{
+			InputFilePath:  root_errorfp + table[1],
+			OutputFilePath: write_filep,
+			LabelName:      table[0],
+			Tokenizer:      "bukt",
+			LabelID:        id,
+			ForceOverwrite: true,
+			LabelFirst:     false,
+			LabelNameFirst: false,
+			AddLabelName:   true,
+			AddLabelID:     false,
+		}
+		smodel.ParseInputWriteOut()
+	}
+
+	cls := knn.NewKnnClassifier("euclidean", 1)
+	rawData, perr0 := base.ParseCSVToInstances(write_filep, true)
+	if perr0 != nil {
+		panic(fmt.Sprintf("parse csv instances error: %s", perr0.Error()))
+	}
+	trainData, _ := base.InstancesTrainTestSplit(rawData, 0.60)
+	cls.Fit(trainData)
+
+	//fmt.Printf("cls: %v\n\n", cls)
+	rawData, perr := base.ParseCSVToInstances(write_filep, true)
+	//fmt.Printf("raw data: %v\n\n", rawData)
+	if perr != nil {
+		panic(fmt.Sprintf("parse csv instances error: %s", perr.Error()))
+	}
+	_, testData := base.InstancesTrainTestSplit(rawData, 0.60)
+
+	predictions := cls.Predict(testData)
+	fmt.Println(predictions)
+
+	confusionMat, err := evaluation.GetConfusionMatrix(testData, predictions)
+	if err != nil {
+		panic(fmt.Sprintf("Unable to get confusion matrix: %s", err.Error()))
+	}
+	fmt.Println(evaluation.GetSummary(confusionMat))
+}
+
+func makeAttrs() {
+	newInst := base.NewDenseInstances()
+	fmt.Println("building new attributes... ")
+	attrs := make([]base.Attribute, 2) //number of attributes
+	attrs[0] = base.NewFloatAttribute("FitValue")
+	attrs[1] = new(base.CategoricalAttribute)
+	attrs[1].SetName("Category")
+
+	fmt.Printf("defined attributes and categorical... %v\n\n", newInst)
+
+	newSpecs := make([]base.AttributeSpec, len(attrs))
+	for i, a := range attrs {
+		fmt.Printf("i: %v\t a: %v\n\n\n", i, a)
+		newSpecs[i] = newInst.AddAttribute(a)
+	}
+	fmt.Printf("added attributes... %v\n\n", newInst)
+
+	newInst.Extend(100)
+
+	fmt.Println(attrs[1].GetType())
+	fmt.Println(attrs[1].GetName())
+	fmt.Println(attrs[1].String())
+	fmt.Println(attrs[0].GetType())
+	fmt.Println(attrs[0].GetName())
+	fmt.Println(attrs[0].String())
+	//fmt.Println(attrs[0].GetSysValFromString("availability_error"))
+}
